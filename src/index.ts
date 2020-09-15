d2fdaba897cf23567020db39e73d5972f3030595
piterowsky <taatarski@gmail.com>
2020-09-15 14:48:10 +0200

    Graphql init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-function hello(name: String = 'World'): String {
-    return `Hello ${name}`;
-}
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
-console.log(hello('Pieter'));
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String!
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  upperHello: () => 'hello world!'.toUpperCase(),
+};
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
